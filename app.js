c62c167a9b5fc2c351cfc11fd0495d4b478da8c2
anilm-500apps <anil.mogili@500apps.com>
2026-06-30 18:22:43 +0530

    Added floating search bar and frequent ragas

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ function filterRagas(query) {
     return carnatic_ragas.filter(raga => raga.name.toLowerCase().includes(query));
 }
 
+function saveUsedRaga(name) {
+    let ragas = JSON.parse(localStorage.getItem("usedRagas")) || [];
+
+    let existing = ragas.find(r => r.name === name);
+
+    if (existing) {
+        existing.count++;
+    } else {
+        ragas.push({ name, count: 1 });
+    }
+
+    ragas.sort((a, b) => b.count - a.count);
+
+    localStorage.setItem("usedRagas", JSON.stringify(ragas));
+}
+
 function onSearch() {
     const query = document.getElementById("search").value;
     const resultsList = document.getElementById("results");
@@ -14,7 +30,11 @@ function onSearch() {
     results.forEach(raga => {
         const li = document.createElement("li");
         li.textContent = raga.name;
-        li.onclick = () => window.location.href = `raga.html?name=${encodeURIComponent(raga.name)}`;
+        // li.onclick = () => window.location.href = `raga.html?name=${encodeURIComponent(raga.name)}`;
+        li.onclick = () => {
+            saveUsedRaga(raga.name);   // increment only here
+            window.location.href = `raga.html?name=${encodeURIComponent(raga.name)}`;
+        };
         resultsList.appendChild(li);
     });
 
